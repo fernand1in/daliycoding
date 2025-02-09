61b9555e38804ebf01a1aaed77bb08eaa830b860
fernand1in <156519510+fernand1in@users.noreply.github.com>
2025-02-09 17:15:33 +0800

    002-1.2
    完善了四个按钮的功能，其中中止和继续按钮有两者表现形式，一种是两者共用一个控件，一种是把控件分开

--- demo01.py
+++ demo01.py
@@ -11,12 +11,7 @@ class QmyWidget(QWidget):
         super().__init__(parent)  # 调用父类构造函数，创建QWidget窗口
         self.ui = Ui_Form()  # 创建登录界面的 UI 对象
         self.ui.setupUi(self)  # 设置 UI
-
-        self.initUI()  # 初始化界面
-
-    def initUI(self):
-        # 绑定登录按钮的点击事件
-        self.ui.pushButton.clicked.connect(self.login)
+        self.ui.pushButton.clicked.connect(self.login) # 绑定登录按钮的点击事件
 
     def login(self):
         # 获取用户名和密码
@@ -24,7 +19,7 @@ class QmyWidget(QWidget):
         password = self.ui.lineEdit_2.text()
 
         # 模拟登录验证
-        if username == "admin" and password == "******":
+        if username == "admin" and password == "123456":
             self.close()  # 关闭登录窗口
             self.open_main_window()  # 打开主窗口
         else:
@@ -37,7 +32,9 @@ class QmyWidget(QWidget):
         self.main_ui.setupUi(self.main_window)
 
         self.main_ui.pushButton_start.clicked.connect(self.init_audio_visualization)
-
+        self.main_ui.pushButton_clear.clicked.connect(self.clear_audio_visualization)
+        self.main_ui.pushButton_stop.clicked.connect(self.stop_audio_visualization)
+        self.main_ui.pushButton_continue.clicked.connect(self.continue_audio_visualization)
         self.main_window.show()
 
     def init_audio_visualization(self):
@@ -77,11 +74,46 @@ class QmyWidget(QWidget):
 
         # 将图表添加到主窗口的 QChartView 控件中
         self.main_ui.QChartView.setChart(self.chart_audio)
+        # 初始化暂停标志
+        self.is_paused = False
+
+    # 实现清空功能
+    def clear_audio_visualization(self):
+        self.chart_audio = QtCharts.QChart()
+        self.series_audio = QtCharts.QLineSeries()
+        self.chart_audio.addSeries(self.series_audio)
+        self.chart_audio.legend().hide()
+        self.audio_axisX = QtCharts.QValueAxis()
+        self.audio_axisX.setRange(0, 2000)
+        self.audio_axisX.setLabelFormat("%g")
+        # self.audio_axisX.setTitleText("Samples")
+        self.audio_axisY = QtCharts.QValueAxis()
+        self.audio_axisY.setRange(-1, 1)
+        # self.audio_axisY.setTitleText("Audio level")
+        self.chart_audio.addAxis(self.audio_axisX, Qt.AlignmentFlag.AlignBottom)
+        self.chart_audio.addAxis(self.audio_axisY, Qt.AlignmentFlag.AlignLeft)
+        self.series_audio.attachAxis(self.audio_axisX)
+        self.series_audio.attachAxis(self.audio_axisY)
+        self.main_ui.QChartView.setChart(self.chart_audio)
+
+    def stop_audio_visualization(self):
+        # 效果一 四个按钮
+        # 切换暂停状态
+        self.is_paused = True
+        # 效果二 三个按钮
+        # self.is_paused = not self.is_paused
+        # if self.is_paused:
+        #     self.main_ui.pushButton_stop.setText("继续")
+        # else:
+        #     self.main_ui.pushButton_stop.setText("暂停")
+
+    def continue_audio_visualization(self):
+        self.is_paused = False
 
     def do_IO_readyRead(self):
-        # Returns the amount of audio data available to read in bytes.
+        if self.is_paused:  # 如果暂停标志为True，则不更新图表
+            return
         bytes = self.m_audioSource.bytesAvailable()
-        # Reads at most maxSize bytes from the device, and returns the data read as a QByteArray.
         buffer = self.ioDevice.read(bytes)
         maxSize = len(buffer)
 
@@ -93,7 +125,7 @@ class QmyWidget(QWidget):
             start = self.sampleCount - availableSamples
             for s in range(start):
                 self.m_buffer[s].setY(self.m_buffer[s + availableSamples].y())
-
+                
         # 添加新的点
         tmp = 0
         for ss in range(start, self.sampleCount):
